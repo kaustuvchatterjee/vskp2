0268dc5777be52de9b56afcfecdcc8aa763b259a
kaustuvchatterjee <75204962+kaustuvchatterjee@users.noreply.github.com>
2022-12-24 18:31:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
