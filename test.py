f6e13d772ad9505d42ce24e45e8b4cd1b5df13dc
kaustuvchatterjee <75204962+kaustuvchatterjee@users.noreply.github.com>
2022-12-24 19:31:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -32,24 +32,24 @@ url = 'https://openweathermap.org/weathermap?basemap=map&cities=false&layer=clou
 Xpath = '//*[@id="map"]/div[1]/div[1]/div[2]/div[2]/*'
 
 browser.get(url)
-element_present = EC.visibility_of_all_elements_located((By.XPATH, Xpath))
-ImageList = WebDriverWait(browser, timeout).until(element_present)
+# element_present = EC.visibility_of_all_elements_located((By.XPATH, Xpath))
+# ImageList = WebDriverWait(browser, timeout).until(element_present)
 
-s = []
-l = []
-t = []
-for element in ImageList:
+# s = []
+# l = []
+# t = []
+# for element in ImageList:
 
-    src = element.get_attribute('src')
-    data = element.get_attribute('style')
-    # print(data)
-    img_data = parseData(data)
-    l.append(img_data[2])
-    t.append(img_data[3])
-    s.append(src)
-    h = img_data[0]
-    w = img_data[1]
-st.text(h,w)
+#     src = element.get_attribute('src')
+#     data = element.get_attribute('style')
+#     # print(data)
+#     img_data = parseData(data)
+#     l.append(img_data[2])
+#     t.append(img_data[3])
+#     s.append(src)
+#     h = img_data[0]
+#     w = img_data[1]
+# st.text(h,w)
 # Footer
 # © 2022 GitHub, Inc.
 # Footer navigation
