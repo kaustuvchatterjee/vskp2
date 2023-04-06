e292a05a62f456ddb43182a88ca789e044d61a99
kaustuvchatterjee <75204962+kaustuvchatterjee@users.noreply.github.com>
2023-04-06 12:36:51 +0530

    Update update_vskpw.py

--- update_vskpw.py
+++ update_vskpw.py
@@ -57,7 +57,8 @@ if x["cod"] != "404":
     # pop = int(f["list"][0]['pop']*100)
 
 # print([dt,temp,tempF,RH,mslp,wind_speed,wind_dir,clouds,rain])
-df1 = pd.DataFrame({'obsDate':dt,'temp':temp,'tempFeel':tempF,'RH':RH,'MSLP':mslp,'windSpeed':wind_speed,'windDir':wind_dir,'clouds':clouds,'rain':rain})
-pd.concat(df, df1)
+y = {'obsDate':[dt],'temp':[temp],'tempFeel':[tempF],'RH':[RH],'MSLP':[mslp],'windSpeed':[wind_speed],'windDir':[wind_dir],'clouds':[clouds],'rain':[rain]}
+df1 = pd.DataFrame(data=y)
+df = pd.concat([df, df1], ignore_index=True)
 # df = df.append({'obsDate':dt,'temp':temp,'tempFeel':tempF,'RH':RH,'MSLP':mslp,'windSpeed':wind_speed,'windDir':wind_dir,'clouds':clouds,'rain':rain}, ignore_index=True)
 df.to_csv('vskpwdata.csv', index=False)
